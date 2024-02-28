33f8267b7d3c31a49b62c6a013302487b3aacc1e
Suliman Tekalli <sulimantekalli@sulimans-mbp.lan>
2024-02-28 11:38:33 -0500

    adjust photo paths

--- src/hero-photos.ts
+++ src/hero-photos.ts
@@ -4,6 +4,6 @@ export const heroPhotos1 = [
 ]
 
 export const heroPhotos2 = [
-  './images/violin-photos/st_violin_4_adjusted_for_hero.jpg',
+  './images/st_violin_4_adjusted_for_hero.jpg',
   './images/adjusted_for_hero_DSCF1963.jpg'
 ]
